fb313f2a129797a70eb4bd7a090a086472471562
Cihan Ogeturk <karoks@gmail.com>
2021-06-17 22:00:03 +1000

    Initial Commit

---
+++ Untitled-1.py
@@ -0,0 +1,21 @@
+import os
+
+os.system('clear')
+
+
+full_name = "Cihan Ogeturk"
+
+print("Hello World written by: " + full_name)
+
+myList = [1, 2, 3, 4]
+
+print(myList)
+
+
+myDict = {
+"cihan" : "1",
+"Arzu" : "2",
+"Mavi" : "3"
+}
+
+print(myDict[0])
