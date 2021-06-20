df86d5c7a083f28e020eb88758644f5c4115f934
Cihan Ogeturk <karoks@gmail.com>
2021-06-20 20:59:28 +1000

    new exercises

--- Code1.py
+++ Code1.py
@@ -3,7 +3,8 @@
 import os
 os.system('clear')
 
-a = 5
-b = 10
 
-print(a+b)
+num = 10
+
+print(float(num))
+
---
+++ ex5.py
@@ -0,0 +1,19 @@
+my_name = 'Zed A. Shaw'
+my_age = 35
+my_height = 74
+my_weight = 180
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He is {my_weight} pounds heavy")
+print("Actually that's not too heavy")
+print(f"He's got {my_eyes} eyes and {my_hair} hair")
+
+total = my_age + my_height + my_weight
+
+print(f"if I add {my_age}, {my_height} and {my_weight}, I get {total}")
+
+
