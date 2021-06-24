f49c0fa64863944f8a1e5e0fb0c9242457da8e55
Cihan Ogeturk <karoks@gmail.com>
2021-06-24 11:47:27 +1000

    New from ubuntu

---
+++ 3.py
@@ -0,0 +1,18 @@
+
+
+import os
+
+
+def function():
+
+	os.system('clear')
+	print(os.getcwd())
+	print("Hello World")
+
+
+function testing system more of a 
+def ():
+	pass
+
+	for import each 2
+	
