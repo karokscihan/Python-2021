0736132e1ddabbd30fbbc8d741c6175307dc29ec
Cihan Ogeturk <karoks@gmail.com>
2021-06-24 21:05:47 +1000

    Test

--- ex5.py
+++ ex5.py
@@ -1,19 +1,9 @@
-my_name = 'Zed A. Shaw'
-my_age = 35
-my_height = 74
-my_weight = 180
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+import tkinter as tk
 
-print(f"let's talk about {my_name}.")
-print(f"He's {my_height} inches tall.")
-print(f"He is {my_weight} pounds heavy")
-print("Actually that's not too heavy")
-print(f"He's got {my_eyes} eyes and {my_hair} hair")
+window = tk.Tk()
 
-total = my_age + my_height + my_weight
+greeting = tk.Label(text="Hello, Tkinter")
+greeting.pack()
 
-print(f"if I add {my_age}, {my_height} and {my_weight}, I get {total}")
-
+window.mainloop()
 
